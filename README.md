92beba0e0d6dfad987a37c8e23104fd823d3a29b
Gianni Salinetti <gsalinet@redhat.com>
2021-03-03 19:43:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ChordScaleGen
+## Random chords and scales Generator
+
+This tiny Python utility creates random chord and scale progressions.
+The lenght of the random progression is choosen by the user.
+Minimal plain text support is given.
+
+**Unix usage:**
+
+    $ python chordscale-generator.py
+
+Plain text files will be dropped here in the working dir (TODO: custom folders).
---
+++ chordscale-generator.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+#    Chords and scales random generator. Version 0.1.
+#    Giovan Battista Salinetti 2015/03/02
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#    This program takes two arguments as input from the user: chord or scale progression and the number of elements
+#    The 'chords' and the 'scales' lists can be expanded with new material without altering the rest of the code
+
+#    TODO:
+#    Export to file facilities (html, csv, xml, improve plain text)
+
+import random
+import textwrap
+
+def tone_prog(n):
+    tone_list = []
+    tones = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"]
+    for t in range(0, n):
+        tone_list.append(random.choice(tones))
+    return tone_list
+
+def chord_prog(n):
+    chord_list = []
+    chords = [ "Major", "Minor", "Diminished", "Augmented", "Maj7", "Min7", "Min/Maj7", "7", "7sus4", "Min7b5", "Dim7", "7alt", "7b9", "Maj7#5", "7#11", "7b5", "7sus4b2", "Maj6", "Min6", "7b9#9", "7#5" ]
+    for c in range(0, int(prog_len)):
+        chord_list.append(random.choice(chords))
+    return chord_list
+
+def scale_prog(n):
+    scale_list = []
+    scales = [ "Major", "Natural Minor", "Melodic Minor", "Harmonic Minor", "Harmonic Major", "Whole Tone", "Half-Whole"]
+    for s in range(0, n):
+        scale_list.append(random.choice(scales))
+    return scale_list
+
+def export_ptext(tn, spec, repeats):
+    name = input("Choose ouput file name, .txt will be appended: ")
+    file_name = name + ".txt"
+    out_file = open( file_name, "w")
+    out_file.write("ChordScaleGen v0.3\n\n")
+    for l in range(0, repeats):
+        out_file.write(tn[l] + " " + spec[l] + "\n")
+    out_file.close
+
+width = 72
+dotted_line = '-' * width
+print(dotted_line)
+print("\033[1m" + "ChordScaleGen" + "\033[0m")
+print("\033[1m" + "Chords and scales generator, Version 0.3" + "\033[0m")
+msg = (
+    "This tool helps musicians to create random sequences "
+    "of chords and scale to practice on instruments. Choose "
+    "between chord or scale generation and the lenght of "
+    "iteration (how much long the sequence will be")
+print(textwrap.fill(msg, width=width))
+print(dotted_line)
+
+mode = input("Generagete chords(c) or scales(s)? ")
+prog_len = input("Set number of iterations: ")
+
+if mode == "c" or mode == "C":
+    print("\033[1m" + "Printing chord chart..." + "\033[0m" + "\n")
+    print(dotted_line)
+    chord_out = chord_prog(int(prog_len))
+    tone_out = tone_prog(int(prog_len))
+    for i in range(0, int(prog_len)):
+         print(tone_out[i], chord_out[i])
+    print(dotted_line)
+    txt_exp = input("\n" + "Export sequence to text file? Yes(y)/No(n) ")
+    if txt_exp == 'y' or txt_exp == 'Y':
+         export_ptext(tone_out, chord_out, int(prog_len))
+
+elif mode == "s" or mode == "S":
+    print("\033[1m" + "Printing scale chart..." + "\033[0m" + "\n")
+    print(dotted_line)
+    scale_out = scale_prog(int(prog_len))
+    tone_out = tone_prog(int(prog_len))
+    for i in range(0, int(prog_len)):
+         print(tone_out[i], scale_out[i])
+    print(dotted_line)
+    txt_exp = input("\n" + "Export sequence to text file? Yes(y)/No(n) ")
+    if txt_exp == 'y' or txt_exp == 'Y':
+         export_ptext(tone_out, scale_out, int(prog_len))
+
+else:
+    print("Wrong choice. You must press \"c/C\" for chords or \"s/S\" for scales.")
+print("Goodbye.")
